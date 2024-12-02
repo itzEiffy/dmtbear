f3abc03208d4e60a24e9eb48a5d5a754939108a1
itzEiffy <113132672+itzEiffy@users.noreply.github.com>
2024-12-02 23:30:00 +0100

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,9 @@
-export const TOKEN_ADDRESS = 'YOUR_TOKEN_ADDRESS_HERE'; // Replace this with your actual token address
+export const TOKEN_ADDRESS = 'ESvLcmvCVt79LBv6BeZE4PdRonxaFjHJAyAaovxXpump'; // Replace this with your actual token address
 
 export const SOCIAL_LINKS = {
   TWITTER: 'https://x.com/DMT_Bear',
   TELEGRAM: 'https://t.me/+aKCo6SKoWPRlNGY0',
-  BUY: 'https://pump.fun',
+  BUY: 'https://pump.fun/coin/ESvLcmvCVt79LBv6BeZE4PdRonxaFjHJAyAaovxXpump',
   DEXSCREENER: 'https://www.dexscreener.com',
   DEXTOOLS: 'https://www.dextools.com'
 } as const;
